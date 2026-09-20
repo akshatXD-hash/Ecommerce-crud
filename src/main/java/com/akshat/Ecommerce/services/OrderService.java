75c47567c70f27505f46507a1d9417cb53bf8176
Akshat <akshatpurohit23dec@gmail.com>
2026-09-20 17:06:34 +0530

    Added Order Service

---
+++ src/main/java/com/akshat/Ecommerce/services/OrderService.java
@@ -0,0 +1,19 @@
+package com.akshat.Ecommerce.services;
+
+import com.akshat.Ecommerce.model.dto.OrderRequest;
+import com.akshat.Ecommerce.model.dto.OrderResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    public static List<OrderResponse> getOrders() {
+
+    }
+
+    public OrderResponse placeOrder(OrderRequest orderRequest) {
+
+    }
+}
