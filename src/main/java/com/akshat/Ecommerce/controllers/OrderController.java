3a75eee8d34108d95360f034409ce60910eaf101
Akshat <akshatpurohit23dec@gmail.com>
2026-09-20 17:05:18 +0530

    Added Order model for the dtos

---
+++ src/main/java/com/akshat/Ecommerce/controllers/OrderController.java
@@ -0,0 +1,35 @@
+package com.akshat.Ecommerce.controllers;
+
+import com.akshat.Ecommerce.model.dto.OrderRequest;
+import com.akshat.Ecommerce.model.dto.OrderResponse;
+import com.akshat.Ecommerce.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+
+    @PostMapping("/orders/place")
+    public ResponseEntity<OrderResponse> placeOrder(@RequestBody OrderRequest orderRequest){
+
+
+        OrderResponse orderResponse = orderService.placeOrder(orderRequest);
+        return new ResponseEntity<>(orderResponse, HttpStatus.CREATED);
+
+    }
+    @GetMapping("/orders")
+    public ResponseEntity<List<OrderResponse>>  getAllOrders(){
+        List<OrderResponse> orders = OrderService.getOrders();
+        return new ResponseEntity<>(orders,HttpStatus.OK);
+    }
+}
